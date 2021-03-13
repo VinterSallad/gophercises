6688f7b7c8deffed471fb36916d1e1252e3fce37
VinterSallad <alexander_huynh@icloud.com>
2021-03-13 14:44:01 +0100

    Implement game loop, STABLE VERSION

--- QuizGame/Quiz.go
+++ QuizGame/Quiz.go
@@ -9,6 +9,12 @@ import (
 	"time"
 )
 
+type gameData struct {
+	difficulty    int
+	corrects      int
+	totalAnswered int
+}
+
 /**
 Takes file to clear and then create a simple math quiz based on user inputted, amount of questions and difficulty.
  */
@@ -94,12 +100,66 @@ func readQuiz(filepath string) [][]string {
 /**
 Initiates game loop from string array representation of games csv file.
  */
-func playQuiz(quiz [][]string) {
+func playQuiz() {
+	filepath := "QuizGame/problems.csv"
 
+	fmt.Println("Welcome to a simple math quiz game where you will \n" +
+					"have 30 seconds to answer as many questions as possible!")
+	fmt.Println("\nLet's start off by you giving a difficulty level to the quiz.")
+	fmt.Println("Please start by typing a number between 1-100")
+
+	correctInput := false
+	questions := 50
+
+	// Failsafe
+	for correctInput == false {
+		var difficulty int
+		fmt.Scanln(&difficulty)
+
+		if difficulty <= 0 || difficulty > 100 {
+			fmt.Println("Please read the instructions!")
+		} else {
+			createQuiz(filepath, questions, difficulty)
+			correctInput = true
+		}
+	}
+
+	// Start line
+	quiz := readQuiz(filepath)
+	fmt.Println("Press enter to start!")
+	fmt.Scanln()
+	var timeOut bool
+
+	// Timer
+	timer := time.AfterFunc(30*time.Second, func() {
+		fmt.Println("30 seconds has passed!")
+		fmt.Println("You may answer the last question.")
+		timeOut = true
+	})
+	defer timer.Stop()
+
+	var correct int
+	var answered int
+	var input int
+
+	// Game loop
+	for i := 0; i < questions && timeOut == false; i++ {
+		fmt.Println(quiz[i][0])
+		fmt.Scanln(&input)
+
+		if strconv.Itoa(input) == quiz[i][1] {
+			correct++
+			answered++
+		} else {
+			answered++
+		}
+	}
+
+	// Results
+	fmt.Println("Correct: ", correct, "/", answered)
 }
 
+// MAIN
 func main() {
-	filepath := "QuizGame/problems.csv"
-	createQuiz(filepath, 10, 10)
-	fmt.Println(readQuiz(filepath))
+	playQuiz()
 }
